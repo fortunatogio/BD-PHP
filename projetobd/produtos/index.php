8cf2368db5774487784badf5f2e4118bfa300178
fortunatogio <giovannifortunato1910@gmail.com>
2024-05-11 10:13:09 -0300

    gio

---
+++ projetobd/produtos/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    require_once("../cabecario.html");
+
+?>
+
+    <h1>Gerenciamento de Produtos</h1>
+
+    <a href="" class="btn btn-primary mt-3">Adicionar Produto</a>
+
+    table
+
+    <?php
+        require_once("../rodape.html");
